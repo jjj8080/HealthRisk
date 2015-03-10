70b79bece36280070f88086c66faf7f01ca73c5a
boyangpan <boyang.pan@hotmail.co.uk>
2015-03-10 13:37:08 +0000

    login php

---
+++ app/src/main/java/com/example/android/effectivenavigation/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.android.effectivenavigation;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://127.0.0.1/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://127.0.0.1/android_login_api/";
+}
--- app/src/main/java/com/example/android/effectivenavigation/LoginActivity.java
+++ app/src/main/java/com/example/android/effectivenavigation/LoginActivity.java
@@ -4,18 +4,73 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.content.Intent;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
 
-/**
- * Created by boyang on 05/03/15.
- */
+
 public class LoginActivity extends Activity {
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+    private Button btnLogin;
+    private Button btnLinkToRegister;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private ProgressDialog pDialog;
+    private SessionManager session;
 
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginpage);
-        TextView textView = (TextView)findViewById(R.id.registrationpage);
+        inputEmail = (EditText)findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+        session = new SessionManager(getApplicationContext());
+        if (session.isLoggedIn()) { //if already logged in, proceed to MainActivity
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                // Check for empty data in the form
+                if (email.trim().length() > 0 && password.trim().length() > 0) {
+                    // login user
+                    checkLogin(email, password);
+                } else {
+                    // Prompt user to enter credentials
+                    Toast.makeText(getApplicationContext(),
+                            "Invalid login details, please retry.", Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+
+        });
+        TextView textView = (TextView)findViewById(R.id.toRegistrationPage);
         textView.setOnClickListener(new View.OnClickListener()
         { public void onClick(View v)
             {
@@ -23,6 +78,85 @@ public class LoginActivity extends Activity {
             LoginActivity.this.startActivity(register);
         }
         });
+    }
+    private void checkLogin(final String email, final String password) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_login";
+
+        pDialog.setMessage("Logging in ...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Method.POST,
+                AppConfig.URL_REGISTER, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Login Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    // Check for error node in json
+                    if (!error) {
+                        // user successfully logged in
+                        // Create login session
+                        session.setLogin(true);
+
+                        // Launch main activity
+                        Intent intent = new Intent(LoginActivity.this,
+                                MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        // Error in login. Get the error message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("tag", "login");
+                params.put("email", email);
+                params.put("password", password);
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 
 }
