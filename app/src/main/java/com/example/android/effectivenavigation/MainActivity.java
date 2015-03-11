3d63d5e1efb5e6dca6b216fa4198eada00c1af46
justinjude <justinjude@me.com>
2015-03-11 11:24:13 +0000

    fixed

--- app/src/main/java/com/example/android/effectivenavigation/MainActivity.java
+++ app/src/main/java/com/example/android/effectivenavigation/MainActivity.java
@@ -18,7 +18,6 @@ package com.example.android.effectivenavigation;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -122,7 +121,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         @Override
         public Fragment getItem(int i) {
             switch (i) {
-                case 0:
+                case 1:
                     // The first section of the app is the most interesting -- it offers
                     // a launchpad into the other demonstrations in this example application.
                     return new LaunchpadSectionFragment();
@@ -167,27 +166,12 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_section_launchpad, container, false);
+            View rootView = inflater.inflate(R.layout.activity_homepage, container, false);
 
             // Demonstration of a collection-browsing activity.
 
 
             // Demonstration of navigating to external activities.
-            rootView.findViewById(R.id.demo_external_activity)
-                    .setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            // Create an intent that asks the user to pick a photo, but using
-                            // FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET, ensures that relaunching
-                            // the application from the device home screen does not return
-                            // to the external activity.
-                            Intent externalActivityIntent = new Intent(Intent.ACTION_PICK);
-                            externalActivityIntent.setType("image/*");
-                            externalActivityIntent.addFlags(
-                                    Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-                            startActivity(externalActivityIntent);
-                        }
-                    });
 
             return rootView;
         }
--- app/src/main/java/com/example/android/effectivenavigation/RegisterActivity.java
+++ app/src/main/java/com/example/android/effectivenavigation/RegisterActivity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 package com.example.android.effectivenavigation;
 
@@ -105,41 +104,3 @@ public class RegisterActivity extends Activity {
 		});
 	}
 }
-=======
-package com.example.android.effectivenavigation;
-import android.app.ActionBar;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-//
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
-
-/**
- * Created by boyang on 05/03/15.
- */
-public class RegisterActivity extends Activity {
-
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getActionBar().setCustomView(R.layout.abs_layout);
-        setContentView(R.layout.registrationpage);
-        TextView textView = (TextView)findViewById(R.id.returntologin);
-        textView.setOnClickListener(new View.OnClickListener()
-        { public void onClick(View v)
-            {
-                Intent register = new Intent(RegisterActivity.this, LoginActivity.class);
-                RegisterActivity.this.startActivity(register);
-            }
-        });
-    }
-}
-
-
-
-
->>>>>>> origin/master
